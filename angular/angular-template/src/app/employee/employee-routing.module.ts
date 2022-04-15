de1756bf896201633ae36c17c1389a8550182928
ducthang0316 <ducthang0316@gmail.com>
2022-04-15 13:59:10 +0700

    merge to security fe

--- angular/angular-template/src/app/employee/employee-routing.module.ts
+++ angular/angular-template/src/app/employee/employee-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuard} from "../sercurity/auth.guard";
+import {EmployeeCreateComponent} from "./employee-create/employee-create.component";
+import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import {EmployeeEditComponent} from "./employee-edit/employee-edit.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'admin/employee',component: EmployeeListComponent, canActivate: [AuthGuard], data:{expectedRole: ['ROLE_ADMIN']}},
+  {path:'admin/employee/update/:id',component: EmployeeEditComponent, canActivate: [AuthGuard], data:{expectedRole: ['ROLE_ADMIN']}},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- angular/angular-template/src/app/entities/entities-routing.module.ts
+++ angular/angular-template/src/app/entities/entities-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EntitiesListComponent} from "./entities-list/entities-list.component";
+import {AuthGuard} from "../sercurity/auth.guard";
+import {EntitiesEditComponent} from "./entities-edit/entities-edit.component";
+import {EntitiesDeleteComponent} from "./entities-delete/entities-delete.component";
+import {EntitiesCreateComponent} from "./entities-create/entities-create.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'employee/entities',component: EntitiesListComponent, canActivate: [AuthGuard], data:{expectedRole: ['ROLE_EMPLOYEE', 'ROLE_ADMIN']}},
+  {path:'employee/entities/update/:id',component: EntitiesEditComponent, canActivate: [AuthGuard], data:{expectedRole: ['ROLE_EMPLOYEE', 'ROLE_ADMIN']}},
+  {path:'employee/entities/delete/:id',component: EntitiesDeleteComponent, canActivate: [AuthGuard], data:{expectedRole: ['ROLE_EMPLOYEE', 'ROLE_ADMIN']}},
+  {path:'employee/entities/create',component: EntitiesCreateComponent, canActivate: [AuthGuard], data:{expectedRole: ['ROLE_EMPLOYEE', 'ROLE_ADMIN']}},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
