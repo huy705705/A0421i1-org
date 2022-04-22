687cf2665381b817604a8b0caf37b712f025e36f
A0421I1-Nguyen Duc Tung <nductung2705@gmail.com>
2022-04-22 21:17:22 +0700

    clear man 2in1

--- demo/src/main/java/com/example/demo/service/impl/RoleServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.service.RoleService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class RoleServiceImpl implements RoleService {
-}
