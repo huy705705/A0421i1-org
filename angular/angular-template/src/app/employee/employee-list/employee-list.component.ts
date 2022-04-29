656ecb04d68b9ea30097d338ac773f3cce1f6dca
DangTu <Dangvantu1998@gmail.com>
2022-04-29 19:09:34 +0700

    Tu commit file

--- angular/angular-template/src/app/employee/employee-list/employee-list.component.ts
+++ angular/angular-template/src/app/employee/employee-list/employee-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {EmployeeService} from '../../service/employee.service';
 import {Router} from '@angular/router';
-import {Employee} from "../../model/employee";
 
 @Component({
   selector: 'app-employee-list',
@@ -25,31 +24,8 @@ export class EmployeeListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.findAllPageable();
     this.search();
   }
-
-  // findAllPageable(){
-  //
-  //   this.isTrue2 = true;
-  //
-  //   this.employeeService.findAllPageable(this.page).subscribe(
-  //     data=>{
-  //       this.employeeList=data['content']
-  //       this.pages= new Array(data['totalPages'])
-  //       this.currentPage= data['currentPage']
-  //     },
-  //     (error) => {
-  //       console.log(error.error.message);
-  //     }
-  //   )
-  // }
-
-  // setPage(page, event: any) {
-  //   event.preventDefault();
-  //   this.page= page;
-  //   this.search();
-  // }
 
   updateEmployee(employee: any) {
     this.router.navigate(['/admin/employee/update', employee.employeeId]);
@@ -63,9 +39,9 @@ export class EmployeeListComponent implements OnInit {
     this.isTrue=true;
     this.employeeService.findAllEmployeeName(this.searchName.trim(),this.searchId.trim(),this.page).toPromise().then(data => {
         console.log(data.length);
-        this.employeeList = data['content']
+        this.employeeList = data.content;
         this.pages = data;
-        this.totalPages= data['totalPages'];
+        this.totalPages= data.totalPages;
         console.log(data);
         this.isSubmitted=true;
         this.isTrue2=true;
